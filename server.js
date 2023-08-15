42b4c5202af08f6982db8d959ea915e84106ed71
Abdul Rahman Hussain Siddique <Abdulrahman.hussain02@gmail.com>
2023-08-15 20:02:43 +0530

    public chat

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { Socket } = require("dgram");
 const express = require("express");
 const app = express();
 
@@ -15,4 +16,15 @@ app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
   //sends our index.html file to browser at port 3000
   res.sendFile(__dirname + "/index.html");
+});
+
+//socket runs on http server
+const io = require("socket.io")(http);
+
+io.on("connection", (socket) => {
+  console.log("Connected...");
+
+  socket.on("message", (msg) => {
+    socket.broadcast.emit('message', msg)
+  });
 });
