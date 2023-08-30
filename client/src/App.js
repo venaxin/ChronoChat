72014ce5353c49b702e622962cdcb5300dfd9ca7
Abdul Rahman Hussain Siddique <Abdulrahman.hussain02@gmail.com>
2023-08-30 21:11:34 +0530

    react-verr

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [msgReceived, setMsgReceived] = useState("");
+  const [room, setRoom] = useState("");
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message, room });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMsgReceived(data.message);
+    });
+  }, [socket]);
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        placeholder="Room no.."
+        onChange={(event) => {
+          setRoom("event.target.value");
+        }}
+      />
+      <button onClick={joinRoom}>Join</button>
+      <input
+        placeholder="Type Message..."
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
+      <button onClick={sendMessage}>Send</button>
+      <h1>message:</h1>
+      {msgReceived}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,1 +1,31 @@
- 
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const app = express();
+const cors = require("cors");
+const server = http.createServer(app);
+
+app.use(cors());
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log("room joined");
+  });
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+    console.log("msg sent");
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server Running");
+});
